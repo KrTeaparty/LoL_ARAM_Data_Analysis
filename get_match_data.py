14194bdff324b819e17cb59c370342527208e7d6
KrTeaparty <hjk125ha@naver.com>
2023-06-19 07:10:46 +0000

    <FEAT> Add new features

--- get_match_data.py
+++ get_match_data.py
@@ -9,20 +9,100 @@ import requests
 import json
 import os
 import pandas as pd
-
+import time
+from urllib import parse
 
 class Riot():
-    def __init__(self):
-        with open('./project/riot/API_KEY.txt', 'r') as f:
+    def __init__(self, server='kr'):
+        with open('API_KEY.txt', 'r') as f:
             self.API_KEYS = f.read().strip()
 
+        self.headers = {
+            'X-Riot-Token': self.API_KEYS
+        }
+        
+        self.server = server # la1이 북미, kr이 한국
+
+    def request_url(self, url):
+        while True:
+            response = requests.get(url, headers=self.headers)
+            code = response.status_code
+            if code == 429:
+                print('Rate Limit! Wait for 60 second')
+                time.sleep(60)
+            if code == 200:
+                break
+            if code == 403:
+                print("Forbidden! Check if API Key is expired.")
+                break
+
+        json_data = response.json()
+        return json_data
+
     def get_summoner_info(self, sum_name):
-        url = 'https://kr.api.riotgames.com/lol/summoners/v4/by-name/'
-        url += sum_name
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-            "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Origin": "https://developer.riotgames.com",
-            "X-Riot-Token": self.API_KEYS
-        }  
+        url = f'https://{self.server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
+        url += parse.quote(sum_name)
+
+        json_data = self.request_url(url)
+        print(json_data)
+
+    def get_diamond_summoners(self, division, page=1):
+        division_dict = {1: 'I', 2:'II', 3:'III', 4:'IV'}
+        url = f'https://{self.server}.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/DIAMOND/'
+        url += division_dict[division] + '?page=' + str(page)
+
+        json_data = self.request_url(url)
+        return json_data
+
+    def get_grandmaster_summoners(self):
+        url = f'https://{self.server}.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5'
+
+        json_data = self.request_url(url)
+        return json_data
+
+    def get_master_summoners(self):
+        url = f'https://{self.server}.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5'
+
+        json_data = self.request_url(url)
+        return json_data
+
+    def get_challenger_summoners(self):
+        url = f'https://{self.server}.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5'
+
+        json_data = self.request_url(url)
+        return json_data
+    
+    def make_summoner_df(self):
+        df = pd.DataFrame()
+        for i in range(1, 5):
+            js = self.get_diamond_summoners(i, 1)
+            df = pd.concat([df, pd.DataFrame(js)], ignore_index=True)
+
+        js = self.get_master_summoners()
+        m = pd.DataFrame(js['entries'])
+        m['tier'] = 'MASTER'
+        df = pd.concat([df, m], ignore_index=True)
+
+        js = self.get_grandmaster_summoners()
+        gm = pd.DataFrame(js['entries'])
+        gm['tier'] = 'GRANDMASTER'
+        df = pd.concat([df, gm], ignore_index=True)
+
+        js = self.get_challenger_summoners()
+        c = pd.DataFrame(js['entries'])
+        c['tier'] = 'CHALLENGER'
+        df = pd.concat([df, c], ignore_index=True)
+
+        df = df[['summonerId','summonerName', 'tier']]
+        df.to_csv('data/summoners.csv', index=False)
+
+
+if __name__ == '__main__':
+
+    r = Riot('kr')
+    #r.get_summoner_info("만 당")
+    #r.get_summoner_by_tier(1, 1)
+    #r.get_grandmaster_summoners()
+    #r.get_master_summoners()
+    #r.make_summoner_df()
+            
